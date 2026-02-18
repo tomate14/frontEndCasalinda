520243613f2244aa7221dbbb18c04e59a6767480
Maximiliano Roselli <maxiroselli@gmail.com>
2026-02-18 12:00:31 -0300

    Nuevos estilos para el sitio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,35 @@ import { NgIf } from '@angular/common';
   imports: [RouterOutlet, ReactiveFormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
 
   title = 'generarPedido';
+  sidebarVisible = true;
   isUsuariosCollapsed = true;
   isComprobanteCollapsed = true;
+
   constructor(private router: Router, private cajaService:CajaService, private confirmarService:ConfirmarService) {}
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
   cambiarComponente(componente: string) {
     if (componente === 'pedidos') {
       this.router.navigate([`/${componente}`, 1]);
     } else if (componente === 'cuentaCorriente') {
       this.router.navigate([`/${componente}`, 2]);
-    }else if (componente === 'ordencompra') {
+    } else if (componente === 'ordencompra') {
       this.router.navigate([`/${componente}`, 3]);
-    }else if (componente === 'ordenventa') {
+    } else if (componente === 'ordenventa') {
       this.router.navigate([`/${componente}`, 4]);
-    }else if (componente === 'notacredito') {
+    } else if (componente === 'notacredito') {
       this.router.navigate([`/${componente}`, 5]);
     } else if (componente === 'clientes') {
       this.router.navigate([`/${componente}`, 1]);
-    }else if (componente === 'proveedores') {
+    } else if (componente === 'proveedores') {
       this.router.navigate([`/${componente}`, 2]);
     } else {
       this.router.navigate([`/${componente}`]);
@@ -42,26 +49,27 @@ export class AppComponent {
   }
 
   cerrarCaja() {
-    const mensaje = "Va a cerrar la caja del dia, esta seguro?";
-    const titulo = "Cerrar caja";
-    this.confirmarService.confirm(titulo, mensaje, false,"Si", "No").then((confirmar)=> {
+    const mensaje = 'Va a cerrar la caja del dia, esta seguro?';
+    const titulo = 'Cerrar caja';
+    this.confirmarService.confirm(titulo, mensaje, false,'Si', 'No').then((confirmar)=> {
       if (confirmar) {
-        const fecha = nowConLuxonATimezoneArgentina(); 
+        const fecha = nowConLuxonATimezoneArgentina();
         const fechaInicio = horaPrincipioFinDia(fecha, false);
         const fechaFin = horaPrincipioFinDia(fecha, true);
         this.cajaService.cierreCaja(fechaInicio, fechaFin).subscribe((res) => {
           if (res.message) {
-            this.confirmarService.confirm("Cierre de caja", res.message, true,"Ok", "");
+            this.confirmarService.confirm('Cierre de caja', res.message, true,'Ok', '');
           } else {
             const saldoCaja = res.contado + res.diferencia;
-            this.confirmarService.confirm("Cierre de caja", `El efectivo en caja deberia ser $${saldoCaja}`, true,"Ok", "");
+            this.confirmarService.confirm('Cierre de caja', `El efectivo en caja deberia ser $${saldoCaja}`, true,'Ok', '');
           }
         }, (error)=> {
-          this.confirmarService.confirm("Error", error.error.text, true,"Ok", "");
-        })
+          this.confirmarService.confirm('Error', error.error.text, true,'Ok', '');
+        });
       }
-    })    
+    });
   }
+
   toggleCollapse(item: string) {
     if (item === 'usuario') {
       this.isUsuariosCollapsed = !this.isUsuariosCollapsed;
